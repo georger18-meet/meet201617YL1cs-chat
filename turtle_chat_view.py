292995e514bfeb5c44bc5075805a0f9360eed101
georger18-meet <goerge1110@gmail.com>
2017-02-27 12:09:48 +0200

    SendButton
    Beginning

--- turtle_chat_view.py
+++ turtle_chat_view.py
@@ -54,6 +54,9 @@ class TextBox(TextInput):
 #   and you can erase that text using
 #
 #   self.writer.clear()
+    def clear_msg(self):
+        self.new_msg=''
+        self.write_msg()
 #
 #3. If you want to make a newline character (i.e. go to the next line), just add
 #   \r to your string.  Test it out at the Python shell for practice
@@ -66,7 +69,8 @@ class TextBox(TextInput):
 #Make a class called SendButton, which will be a subclass of Button.
 #Button is an abstract class with one abstract method: fun.
 #fun gets called whenever the button is clicked.  It's jobs will be to
-#
+class SendButton(Button):
+    def fun(self,x=None,y=None):
 # 1. send a message to the other chat participant - to do this,
 #    you will need to call the send method of your Client instance
 # 2. update the messages that you see on the screen
